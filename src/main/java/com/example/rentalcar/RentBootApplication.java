bcf47a144a437db171f5142e376fbcc57b067203
verishkoalexey <Verishkoalexey@gmail.com>
2020-02-29 11:52:59 +0300

    commit3

--- src/main/java/com/example/rentalcar/RentBootApplication.java
+++ src/main/java/com/example/rentalcar/RentBootApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RentBootApplication {
-	
+	/* A new application*/
 	
 
 	public static void main(String[] args) {
